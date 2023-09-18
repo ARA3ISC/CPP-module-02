1a0b55649147c1bdff4f1b6c5f1463469b3b0a06
ara3isc <m.aneddame01@gmail.com>
2023-09-18 20:37:06 +0100

    working on ex02

---
+++ ex02/Fixed.cpp
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maneddam <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 20:24:23 by maneddam          #+#    #+#             */
+/*   Updated: 2023/09/18 20:25:47 by maneddam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+
+Fixed::Fixed():raw(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& old)
+{
+	this->raw = old.raw;
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& old)
+{
+	if (this != &old)
+		this->raw = old.raw;
+	std::cout << "Copy assignment operator called" << std::endl;
+	return *this;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	// std::cout << "getRawBits member function called" << std::endl;
+	return (this->raw >> 8);
+}
+
+void Fixed::setRawBits( int const _raw )
+{
+	// std::cout << "setRawBits member function called" << std::endl;
+	this->raw = _raw;
+}
+
+Fixed::Fixed(const int _raw)
+{
+	this->raw = _raw << this->fbitsnum;
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float _raw)
+{
+	this->raw = roundf(_raw * 256);
+	std::cout << "Float constructor called" << std::endl;
+}
+
+float Fixed::toFloat( void ) const
+{
+	return ((float)this->raw / 256);
+}
+
+int Fixed::toInt( void ) const
+{
+	return (this->raw / 256);
+}
+
+std::ostream& operator<<(std::ostream& output, const Fixed& myObj)
+{
+	output << myObj.toFloat();
+	return output;
+}
+
---
+++ ex02/Fixed.hpp
@@ -0,0 +1,35 @@
+#ifndef FIXED_HPP
+#define FIZED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+private:
+	int raw;
+	static const int fbitsnum = 8;
+
+public:
+	Fixed();
+	Fixed(const Fixed &old);
+	Fixed &operator=(const Fixed &old);
+	~Fixed();
+
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+
+
+	Fixed(const int _raw);
+	Fixed(const float _raw);
+	float toFloat( void ) const;
+	int toInt( void ) const;
+
+
+	// Fixed& 
+
+};
+
+std::ostream& operator<<(std::ostream& _cout, const Fixed& myObj);
+
+#endif
