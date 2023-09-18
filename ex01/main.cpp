465cdaf7fdbc9088af1102cc85659ed9f8097431
ara3isc <m.aneddame01@gmail.com>
2023-09-18 20:13:54 +0100

    ex01 done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: maneddam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 15:37:27 by maneddam          #+#    #+#             */
-/*   Updated: 2023/09/18 19:53:21 by maneddam         ###   ########.fr       */
+/*   Updated: 2023/09/18 20:13:21 by maneddam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,21 @@
 int main() {
 
 	Fixed		a;
-	// Fixed const	b(10);
+	Fixed const	b(10);
 	Fixed const c(42.42f);
-	// Fixed const	d(b);
+	Fixed const	d(b);
 
 
-	// a = Fixed(1234.4321f);
+	a = Fixed(1234.4321f);
 
-	std::cout << c << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
 
-	// std::cout << "a is " << a.getRawBits() << std::endl;
-	// std::cout << "b is " << b.getRawBits() << std::endl;
-	// std::cout << "c is " << c.getRawBits() << std::endl;
-	// std::cout << "d is " << d.getRawBits() << std::endl;
+	std::cout << "a is " << a.toInt() << " as an integer "<< std::endl;
+	std::cout << "b is " << b.toInt() << " as an integer "<< std::endl;
+	std::cout << "c is " << c.toInt() << " as an integer "<< std::endl;
+	std::cout << "d is " << d.toInt() << " as an integer "<< std::endl;
 	return 0;
 }
-
-
